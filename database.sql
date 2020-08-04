ff2ebc7cf45b60d03dc5600e799f3fd31bd290af
zhangds <zhudachang0832@gmail.com>
2020-08-04 17:02:09 +0800

    更新

--- database.sql
+++ database.sql
@@ -3,3 +3,6 @@ SELECT * from tb_users WHERE tel='13438888888'
 
 -- 查询icon数据
 SELECT * FROM bill_type_icon;
+
+-- 查询账单数据
+SELECT * from bill_money_list_log,bill_detail_list  WHERE 1234=bill_detail_list.bill_money_log_id
